010be3c43538d423806890aa3d88034b53135940
Vytautas K <makrelion@gmail.com>
2023-01-16 15:01:25 +0200

    applied visual changes

--- mysite/autoservisas/models.py
+++ mysite/autoservisas/models.py
@@ -34,6 +34,19 @@ class Uzsakymas(models.Model):
     data = models.DateField('Uzsakymo data', null=True, blank=True)
     automobilis_id = models.ForeignKey('Automobilis', on_delete=models.SET_NULL, null=True)
     suma = models.FloatField('Suma', max_length=10, help_text='Iveskite paslaugos kaina', default=0)
+    LOAN_STATUS = (
+        ('a', 'Uzsakymas gautas'),
+        ('p', 'Uzsakymas priimtas'),
+        ('g', 'Uzsakymas vykdomas'),
+        ('r', 'Uzsakymas baigtas')
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='a',
+        help_text='Status'
+    )
 
     class Meta:
         ordering = ['data']
@@ -69,66 +82,3 @@ class Modelis(models.Model):
 
     def __str__(self):
         return f'{self.marke}, {self.modelis}'
-
-#
-# class Genre(models.Model):
-#     name = models.CharField('Pavadinimas', max_length=200, help_text='Iveskite knygos zanra')
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Book(models.Model):
-#     title = models.CharField('Pavadinimas', max_length=200)
-#     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-#     summary = models.TextField('Aprasymas', max_length=1000, help_text='Trumpas knygos aprasymas')
-#     isbn = models.CharField('ISBN', max_length=13)
-#     # Rysys daug su daug
-#     genre = models.ManyToManyField(Genre, help_text='Isrinkite zanra knygai')
-#
-#     def _str__(self):
-#         return self.title
-#
-#     # Gauname linka
-#     def absolute_url(self):
-#         return reverse('book-detail', args=[str(self.id)])
-#
-#
-# class BookInstance(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unikalus knygos kopijos ID')
-#     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-#     due_back = models.DateField('Bus prieinama', null=True, blank=True)
-#     LOAN_STATUS = (
-#         ('a', 'Administruojama'),
-#         ('p', 'Paimta'),
-#         ('g', 'Galima paimti'),
-#         ('r', 'Rezervuota')
-#     )
-#     status = models.CharField(
-#         max_length=1,
-#         choices=LOAN_STATUS,
-#         blank=True,
-#         default='a',
-#         help_text='Status'
-#     )
-#
-#     # nustatomas rykiavimas pagal (due back)
-#     class Meta:
-#         ordering = ['due_back']
-#
-#     def __str__(self):
-#         return f'{self.id} {self.book.title}'
-#
-#
-# class Author(models.Model):
-#     first_name = models.CharField('Vardas', max_length=100)
-#     last_name = models.CharField('Pavarde', max_length=100)
-#
-#     class Meta:
-#         ordering = ['last_name', 'first_name']
-#
-#     def get_absolute_url(self):
-#         return reverse('author-detail', args=[str(self.id)])
-#
-#     def __str__(self):
-#         return f'{self.last_name}, {self.first_name}'
--- mysite/autoservisas/views.py
+++ mysite/autoservisas/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Uzsakymas, UzsakymoEilute, Modelis
 
 def index(request):
-    return HttpResponse("Labas, autoserviso naudotojau!")
+    num_uzsakymas = Uzsakymas.objects.all().count()  # is models.py, Book klases
+    num_uzsakymo_eilute = UzsakymoEilute.objects.all().count()
+    num_modelis = Modelis.objects.all().count()
+    # Filtruojame is kintamojo status 'g' reiksme
+    num_instances_g = Uzsakymas.objects.filter(status__exact='g').count()
+    context = {'num_uzsakymas': num_uzsakymas,
+               'num_uzsakymo_eilute': num_uzsakymo_eilute,
+               'num_modelis': num_modelis,
+               'num_instances_g': num_instances_g}
+    return render(request, 'index.html', context=context)
 
 # Create your views here.
